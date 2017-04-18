7dd16d2cde6d739e16fff193398df91182736d96
Alexander Kaluzhny <alexander.kaluzhny@gmail.com>
2017-04-18 23:34:57 +0300

    some security settings temporary disabled for heroku.

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -11,3 +11,7 @@ EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.c
 
 # Logging into console instead of mail_admins
 LOGGING['loggers']['django.request']['handlers'] = ['console']
+
+# disabled for test purposes
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
