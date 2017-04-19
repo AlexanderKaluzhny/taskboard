ed7ef38e9b43aad58047e1cbcedb454a9badc587
Alexander Kaluzhny <alexander.kaluzhny@gmail.com>
2017-04-19 22:40:55 +0300

    redundant update method code removed.

--- task_board/api/tasks/views.py
+++ task_board/api/tasks/views.py
@@ -159,27 +159,6 @@ class TaskUpdateDeleteView(mixins.DestroyModelMixin, UpdateAPIView):
     renderer_classes = (JSONRenderer,)
     permission_classes = (IsAuthenticated, IsTaskOwnerOrMarkDoneOnly,)
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        # data = request.data.copy()
-
-        # specify the user who accomplished the task if the status 'done' is set
-        # if ('status' in request.data.keys()
-        #     and str(request.data['status']) == str(Task.STATUS_DONE)):
-        #     data['accomplished_by'] = request.user.pk
-
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
-
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
