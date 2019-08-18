df73639111136feabc8e45dede57c34979a8f944
Alexander Kaluzhny <alexander.kaluzhny@gmail.com>
2019-08-18 17:05:46 +0300

    react build files committed, to avoid building on heroku.

---
+++ frontend/build/root/precache-manifest.18ecd9dd16e55b297a98c085ecbf8804.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e0ecc1df6a34eef05c83922a5e2c67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9f64d8260db6bae706f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a546c2a9d3fc92cec83a",
+    "url": "/static/js/2.2a6562a4.chunk.js"
+  },
+  {
+    "revision": "f9f64d8260db6bae706f",
+    "url": "/static/js/main.d63dc85d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
